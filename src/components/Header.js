d7a9095468868eecc7ca9e002d155c78b9e9e723
Jonathan Lee <51lverfangster@gmail.com>
2018-07-29 23:28:15 -0700

    Add date info

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,7 @@ type Props = {
 export default class Header extends React.PureComponent<Props> {
   state: {} = {
     currentSelection: 'All Campaigns',
+    currentDate: new Date(),
   };
 
   updateCurrentSelection = (e) => {
@@ -32,22 +33,40 @@ export default class Header extends React.PureComponent<Props> {
     });
   }
 
+  renderCurrentDate = () => {
+    const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const { currentDate } = this.state;
+
+    return (
+      <div className="text-danger">
+        <i className="far fa-calendar" />
+        &nbsp;Today,&nbsp;
+        {shortMonths[currentDate.getMonth()]}
+        &nbsp;
+        {currentDate.getDate()}
+      </div>
+    );
+  }
+
   render() {
+    const { currentDate } = this.state;
+    console.log(currentDate);
     const { currentSelection } = this.state;
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light" key='header'>
         <div className="container">
+
           <div className="dropdown">
             <button className="btn btn-light dropdown-toggle border" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               { currentSelection }
             </button>
-
             <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
               <div className="dropdown-item" onClick={this.updateCurrentSelection} >All Campaigns</div>
               {this.renderCampaignsList()}
             </div>
-
           </div>
+
+          {this.renderCurrentDate()}
         </div>
       </nav>
     );
